58b26a7df9429174ac9334015aa66c649dc7a3f8
Junmoo Byun <107118729+JunmooB@users.noreply.github.com>
2023-11-23 14:45:31 +0900

    [Silver V] Title: 올림픽, Time: 52 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,29 +3,26 @@ input = sys.stdin.readline
 
 n , k = map(int, input().split())
 medals = [list(map(int, input().split())) for _ in range(n)]
-
-rank_list = []
-results = sorted(medals, key = lambda x:[-x[1], -x[2], -x[3]])
 rank = 1
-for i in range(len(results) -1):
-    cnt = 1
-    while True:
-        if results[i][1] == results[i+cnt][1]:
-            if results[i][2] == results[i+cnt][2]:
-                if results[i][3] == results[i+cnt][3]:
-                    cnt += 1
-                else:
-                    break
-            else:
-                break
-        else:
-            break
-    for j in range(cnt):
-        rank_list.append(rank)
-    rank += cnt
 
-for result in results:
-    if result[0] == k:
-        ind = results.index(result)
+for medal in medals:
+    if medal[0] == k:
+        gold = medal[1]
+        silver = medal[2]
+        bronze = medal[3]
+        medals.remove(medal)
         break
-print(rank_list[ind-1])
+
+for medal in medals:
+    if medal[1] > gold:
+        rank += 1
+        continue
+    elif medal[1] == gold:            
+        if medal[2] > silver:
+            rank += 1
+            continue
+        elif medal[2] == silver:            
+            if medal[3] > bronze:
+                rank += 1
+                continue
+print(rank)
