399beac16a73db1be9fb72e688631b4d380f13a8
Junmoo Byun <107118729+JunmooB@users.noreply.github.com>
2023-11-29 15:30:18 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,14 +1,17 @@
 import sys
 input = sys.stdin.readline
 
+
 tanos = list(input()[:-1])
 zero_cnt = tanos.count('0')
 one_cnt = tanos.count('1')
-for _ in range(zero_cnt//2):
-    tanos.remove('0')
+
 for _ in range(one_cnt//2):
     tanos.remove('1')
+tanos.reverse()
+for _ in range(zero_cnt//2):
+    tanos.remove('0')
+tanos.reverse()
 
-tanos.sort()
 for text in tanos:
     print(text, end='')
