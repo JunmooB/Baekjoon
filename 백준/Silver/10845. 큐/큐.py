4e6c158169c03752cc723f33b3e187488eb2792c
Junmoo Byun <107118729+JunmooB@users.noreply.github.com>
2024-01-04 13:56:49 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 34220 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+def function(deque, command):
+    if command[:4] == 'push':
+        command , num = command.split()
+        deque.append(num)
+    elif command == 'top':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[len(deque)-1])
+    elif command == 'pop':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.popleft())
+    elif command == 'size':
+        print(len(deque))
+    elif command == 'empty':
+        if len(deque) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command == 'front':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[0])
+    elif command == 'back':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[len(deque)-1])
+
+
+    return deque
+
+n = int(input())
+q = deque()
+for _ in range(n):
+    command = str(input().strip())
+    q = function(q, command)
