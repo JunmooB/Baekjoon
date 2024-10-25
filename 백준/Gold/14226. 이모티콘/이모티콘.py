b84d8a058648f0ed458133ce851b36340fc95c84
Junmoo Byun <107118729+JunmooB@users.noreply.github.com>
2024-10-25 14:20:21 +0900

    [Gold IV] Title: 이모티콘, Time: 88 ms, Memory: 39344 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -5,7 +5,7 @@ from collections import deque
 
 s = int(input())
 
-visited = [[False] * 10001 for _ in range(10001)]
+visited = [[False] * 1001 for _ in range(1001)]
 # 위치 node, clip, time
 q = deque([(1, 0, 0)])
 visited[1][0] = True
@@ -19,11 +19,11 @@ while q:
         if not visited[now][now]:
             visited[now][now] = True
             q.append((now, now, time + 1))
-        if not visited[now+clip][clip]:
-            visited[now+clip][clip] = True
-            q.append((now+clip, clip, time + 1))
-        if not visited[now-1][clip]:
-            visited[now-1][clip] = True
-            q.append((now-1, clip, time + 1))
-            
-        
+        if now + clip < 1001:
+            if not visited[now+clip][clip]:
+                visited[now+clip][clip] = True
+                q.append((now+clip, clip, time + 1))
+        if 1<= now-1 < 1001:
+            if not visited[now-1][clip]:
+                visited[now-1][clip] = True
+                q.append((now-1, clip, time + 1))
